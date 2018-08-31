911b66a4547fcdf1ebbb481c55aae2916c983d6e
Jason Schwartz <jasonscottschwartz@gmail.com>
2018-08-31 13:15:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   } else {
     totalCost = total();
-    cart.splice(0,cart.length-1);
+    cart.splice(0,cart.length);
     return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`
   }
 }
