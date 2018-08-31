b8735fb44c673b1d4ceed185bb331befdeadd1d3
Jason Schwartz <jasonscottschwartz@gmail.com>
2018-08-31 12:38:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,7 +42,11 @@ function viewCart() {
 
 function total() {
   // write your code here
-  
+  var total;
+  for (let i = 0; i<cart.length; i++){
+    total += cart[i].itemPrice;
+  }
+  return total;
 }
 
 function removeFromCart(item) {
